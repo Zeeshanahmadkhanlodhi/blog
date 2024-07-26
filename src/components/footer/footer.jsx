3ebf061268cf7b8a21190708b810dae2bb98a858
zeeshanahmadkhanlodhi <zeeshan22ahmad33@gmail.com>
2024-07-26 10:57:32 -0700

    First commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./footer.module.css";
+
+
+const footer = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default footer
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./navbar.module.css";
+
+
+const navbar = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default navbar
